f94a8824ac1f22a39384ba3f81d779bc4129467e
Aidan van Geest <aidanvangeest@gmail.com>
2023-05-09 12:18:35 +0200

    Added Dave's examples

--- src/main/java/com/booleanuk/core/CohortManager.java
+++ src/main/java/com/booleanuk/core/CohortManager.java
@@ -1,5 +1,18 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class CohortManager {
+
+    //sayHello example
+    public String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    public boolean search(ArrayList<String> cohorts, String name) {
+        return cohorts.contains(name);
+    }
+
+
 
 }
