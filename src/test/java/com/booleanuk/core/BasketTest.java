06aee9140d8e4eb0c01988377c5c82403ef0a2a0
Farhang Younis <comp-sci@hotmail.com>
2023-05-16 20:52:48 +0200

    Fixed an error in testTotal()

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -28,7 +28,8 @@ public class BasketTest {
         // when not empty
         basket.addItems("Product A", 10);
         basket.addItems("Product B", 20);
-
+        expected=30;
+        actual = basket.total();
         Assertions.assertEquals(expected,actual);
     }
 
