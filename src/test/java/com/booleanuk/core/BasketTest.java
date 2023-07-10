3a13a7e5388d32e3322e167942ca478d57b9bea3
himok <himok.pk@gmail.com>
2023-07-10 11:54:05 +0200

    Creating addExistingItemToBasket Test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -2,6 +2,7 @@ package com.booleanuk.core;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasketTest {
@@ -13,5 +14,15 @@ public class BasketTest {
         int price = 12;
 
         assertTrue(basket.add(product, price));
+    }
+
+    @Test
+    public void addExistingItemToBasket() {
+        Basket basket = new Basket();
+        String product = "Apple";
+        int price = 12;
+
+        assertTrue(basket.add(product, price));
+        assertFalse(basket.add(product, price+1));
     }
 }
