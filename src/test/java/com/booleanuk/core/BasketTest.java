bd975095f20be84e1be05e96fea420ffc4c3809b
Anastasiya <ronas0709@gmail.com>
2023-07-10 12:09:38 +0200

    add tests

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -24,4 +24,32 @@ class BasketTest {
 
         Assertions.assertEquals(5,newBasket.total());
     }
+    @Test
+    public void testAddExistingItem(){
+        Basket newBasket = new Basket();
+        newBasket.add("Bread",2);
+        boolean result = newBasket.add("Bread",5);
+        Assertions.assertFalse(result);
+
+    }
+    @Test
+    public void testCalculateTotalWithEmptyBasket(){
+        Basket newBasket = new Basket();
+        int total = newBasket.total();
+        Assertions.assertEquals(0,total);
+
+    }
+    @Test
+    public void testAddItemWithSameNameDiffrentPrice(){
+        Basket newBasket = new Basket();
+        boolean result1 = newBasket.add("Apple",10);
+        boolean result2 = newBasket.add("Apple",15);
+        Assertions.assertTrue(result1);
+        Assertions.assertFalse(result2);
+
+
+
+    }
+
+
 }
