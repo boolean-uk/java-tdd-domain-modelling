ef7dbff5258bcdf965172972acdd493186230ed5
Rafal Halama <rafau.halama@gmail.com>
2023-07-10 11:49:58 +0200

    tests passed for add()

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,4 +1,16 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class Basket {
+    HashMap<String, Integer> items = new HashMap<>();
+    public boolean add(String product, int price) {
+        if(items.containsKey(product)){
+            return false;
+        }
+        else{
+            items.put(product, price);
+            return true;
+        }
+    }
 }
