66e0966fef2ff624af71e5f893e7b2f8568d3805
Martyna <martyna.marszalek@talan.com>
2023-07-10 12:30:12 +0200

    Martyna Marszałek, Marlena Łuczak

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,4 +1,31 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class Basket {
+    public HashMap<String, Integer> items = new HashMap<>();
+
+    public boolean add(String product, int price) {
+        if (items.containsKey(product)) {
+            return false;
+        }
+        items.put(product, price);
+        return true;
+    }
+
+    public int total() {
+        int sum = 0;
+        for (int price : items.values()) {
+            sum += price;
+        }
+        return sum;
+    }
+
+//    public static void main(String[] args ){
+//        Basket basket = new Basket();
+//        for (String i: basket.items.keySet()
+//        ) {
+//            System.out.println(i);
+//        }
+//    }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -10,27 +10,41 @@ import java.util.HashMap;
 public class BasketTest {
     private Basket basket;
 
-    //add(String product, int price)
+    @BeforeEach
+    void setBasket(){
+        basket = new Basket();
+    }
     @Test
     public void testAddReturnsFalse(){
-        HashMap<String, Integer> items = new HashMap<>();
-        items.put("Milk",2);
-        Assertions.assertFalse(basket.add("Milk",2));
+        //HashMap<String, Integer> items = new HashMap<>();
+        basket.items.put("Milk",2);
+        boolean result = basket.add("Milk",2);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void testAddReturnsFalse2(){
+        //HashMap<String, Integer> items = new HashMap<>();
+        basket.items.put("Flour",2);
+        boolean result = basket.add("Flour",2);
+        Assertions.assertFalse(result);
     }
 
     @Test
     public void testAddReturnsTrue(){
-        HashMap<String, Integer> items = new HashMap<>();
-        items.put("Juice",3);
-        items.put("Milk",2);
-        Assertions.assertFalse(basket.add("Orange",2));
+        //HashMap<String, Integer> items = new HashMap<>();
+        basket.items.put("Juice",3);
+        basket.items.put("Milk",2);
+        Assertions.assertTrue(basket.add("Orange",2));
     }
 
     @Test
     public void testTotal(){
-        HashMap<String, Integer> items = new HashMap<>();
-        items.put("Juice",3);
-        items.put("Milk",2);
+       // HashMap<String, Integer> items = new HashMap<>();
+       // items.put("Juice",3);
+       // items.put("Milk",2);
+        basket.items.put("Milk",2);
+        basket.items.put("Juice",3);
         Assertions.assertEquals(5, basket.total());
     }
 
