55702e532ccaf5771bc6ff03378c1bc8fa7c0910
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-10 12:35:19 +0200

    repairing little mistake

--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -4,44 +4,5 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CohortManagerTest {
-    @Test
-    public void whenItemIsNotInTheBasketShouldReturnTrue(){
-        Basket basket = new Basket();
-        String product = "Milk";
-        int price = 14;
 
-        boolean result = basket.add(product, price);
-
-        Assertions.assertTrue(result);
-    }
-
-    @Test
-    public void whenItemIsInTheBasketShouldReturnFalse(){
-        Basket basket = new Basket();
-        String product = "Milk";
-        int price = 14;
-
-        basket.add(product, price);
-
-        boolean result = basket.add(product, price);
-
-        Assertions.assertFalse(result);
-    }
-
-    @Test
-    public void returnTotalPriceOfItems(){
-        Basket basket = new Basket();
-
-        String product1 = "Milk";
-        int price1 = 14;
-        basket.add(product1, price1);
-
-        String product2 = "Butter";
-        int price2 = 36;
-        basket.add(product2, price2);
-
-        int result = basket.total();
-
-        Assertions.assertEquals(result, 50);
-    }
 }
