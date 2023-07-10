f477adfbd604c71433e52f25912f65f1ef50f152
DorotaWlazlo <dori.wlazlo@gmail.com>
2023-07-10 12:07:47 +0200

    functionalities created

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -5,4 +5,21 @@ import java.util.Map;
 
 public class Basket {
     private Map<String, Integer> items = new HashMap<>();
+
+    public boolean add(String product, int price){
+        if(items.containsKey(product)){
+            return false;
+        }else{
+            items.put(product, price);
+            return true;
+        }
+    }
+
+    public int total(){
+        int total = 0;
+        for(Map.Entry<String, Integer> entry : items.entrySet()){
+            total+= entry.getValue();
+        }
+        return total;
+    }
 }
