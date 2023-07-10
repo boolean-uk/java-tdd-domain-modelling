a5b0648af06c41f7336bd87eaafff5e9177992b5
Julia Girejko <julia.girejko@talan.com>
2023-07-10 12:02:39 +0200

    code commit

--- src/main/java/com/booleanuk/core/CohortManager.java
+++ src/main/java/com/booleanuk/core/CohortManager.java
@@ -1,5 +1,20 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class CohortManager {
 
+    private HashMap<String, Integer> list = new HashMap<>();
+    private int totalPrice;
+    public void add(String product, int price){
+        list.put(product, price);
+    }
+
+    public int total(){
+        return list.values().stream().mapToInt(d-> d).sum();
+    }
+
+    public boolean contains(String product){
+        return list.containsKey(product);
+    }
 }
