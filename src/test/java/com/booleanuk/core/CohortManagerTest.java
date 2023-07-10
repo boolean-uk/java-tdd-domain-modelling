8a9d0b2df23fe57efc8b7f074db52653e95b467a
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-10 12:03:34 +0200

    red stage of third test

--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -14,4 +14,34 @@ class CohortManagerTest {
 
         Assertions.assertTrue(result);
     }
+
+    @Test
+    public void whenItemIsInTheBasketShouldReturnFalse(){
+        Basket basket = new Basket();
+        String product = "Milk";
+        int price = 14;
+
+        basket.add(product, price);
+
+        boolean result = basket.add(product, price);
+
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void returnTotalPriceOfItems(){
+        Basket basket = new Basket();
+
+        String product1 = "Milk";
+        int price1 = 14;
+        basket.add(product1, price1);
+
+        String product2 = "Butter";
+        int price2 = 36;
+        basket.add(product2, price2);
+
+        int result = basket.total();
+
+        Assertions.assertEquals(result, 50);
+    }
 }
