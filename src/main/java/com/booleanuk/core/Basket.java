1051f761e371687d8f122b5ad31850978bc42c33
karim <01171252@pw.edu.pl>
2023-07-10 11:58:47 +0200

    1 test

---
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -0,0 +1,16 @@
+package com.booleanuk.core;
+
+import java.util.HashMap;
+
+public class Basket {
+    private HashMap<String, Integer> items;
+
+    public Basket() {
+    }
+
+    public boolean add(String product, int price){
+        return false;
+    }
+
+    public int total(){return -1;}
+}
--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -3,6 +3,15 @@ package com.booleanuk.core;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CohortManagerTest {
+    @Test
+    public void shouldAddProductWhenNotExists(){
+        Basket basket = new Basket();
+        boolean isInBasket = basket.add("aaa", 22);
+
+        assertEquals(true, isInBasket);
+    }
 
 }
