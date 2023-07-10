c3df0c59c8695f0ee80d3da46fcfd1555d09acd8
himok <himok.pk@gmail.com>
2023-07-10 12:15:55 +0200

    Adding searchNonExistingItem in cohortManagerTests

--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -7,6 +7,7 @@ import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CohortManagerTest {
@@ -19,5 +20,16 @@ class CohortManagerTest {
             add("c");
         }};
         assertTrue(cohortmanager.search(cohort, "a"));
+    }
+
+    @Test
+    public void searchNonExistingItem() {
+        CohortManager cohortmanager = new CohortManager();
+        List<String> cohort = new ArrayList<>() {{
+            add("a");
+            add("b");
+            add("c");
+        }};
+        assertFalse(cohortmanager.search(cohort, "d"));
     }
 }
