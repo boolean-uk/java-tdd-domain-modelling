b0f28311859fc0b39b1092f7572e08c1c4fe54ab
Piotr Karolak <karolak.piotr.012@gmail.com>
2023-07-10 11:54:58 +0200

    Added itemWithTheProvidedNameIsAlreadyInTheBasketShouldReturnFalse test

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,10 +1,16 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class Basket {
+    public HashMap<String, Integer> items = new HashMap<>();
 
 
-
-    boolean add(String product, int price){
-        return false;
+    public boolean add(String product, int price){
+        if (items.containsKey(product)){
+            return false;
+        }
+        items.put(product,price);
+        return true;
     }
 }
--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,10 +1,23 @@
 package com.booleanuk.core;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BasketTest
 {
 
+    @Test
+    public void itemWithTheProvidedNameIsAlreadyInTheBasketShouldReturnFalse(){
+        // when
+        Basket basket = new Basket();
+        basket.items.put("tomato",2);
+        basket.items.put("potato",1);
+        //given
+        boolean isInTheBasket = basket.add("tomato",2);
+
+        //then
+        Assertions.assertFalse(isInTheBasket);
+    }
 
 }
