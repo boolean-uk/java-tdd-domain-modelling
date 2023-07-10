0a8ecb0cfd8bb26e4d01d7cb99b954b53027b828
Alicja Bobko <alicja.bobko@talan.com>
2023-07-10 11:38:20 +0200

    cohort manager example

--- src/main/java/com/booleanuk/core/CohortManager.java
+++ src/main/java/com/booleanuk/core/CohortManager.java
@@ -1,5 +1,11 @@
 package com.booleanuk.core;
 
+import java.util.List;
+
 public class CohortManager {
+
+    public boolean search(List<String>cohorts, String name){
+       return cohorts.contains(name);
+    }
 
 }
