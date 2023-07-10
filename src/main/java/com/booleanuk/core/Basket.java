fbe38f7347e30824e0344640548492223d0dc033
himok <himok.pk@gmail.com>
2023-07-10 11:56:54 +0200

    Solving addExistingItemToBasket test

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,7 +1,18 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class Basket {
+    HashMap<String, Integer> items;
+
+    public Basket() {
+        this.items = new HashMap<>();
+    }
+
     public boolean add(String product, int price) {
+        if(this.items.containsKey(product))
+            return false;
+        this.items.put(product, price);
         return true;
     }
 }
