dec2e99470adaf86e33801fe4f1c0fd3b7203d3d
MiloszSobczyk <m.sobczyk.official@gmail.com>
2023-07-10 12:08:31 +0200

    Added code and tests

--- src/main/java/com/booleanuk/core/CohortManager.java
+++ src/main/java/com/booleanuk/core/CohortManager.java
@@ -1,5 +1,20 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+
 public class CohortManager {
 
+    private final HashMap<String, Integer> productsPrice = new HashMap<String, Integer>();
+    private int totalPrice;
+    public void add(String product, int price){
+        productsPrice.put(product, price);
+    }
+
+    public int total(){
+        return productsPrice.values().stream().mapToInt(d-> d).sum();
+    }
+
+    public boolean contains(String product){
+        return productsPrice.containsKey(product);
+    }
 }
--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -4,5 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CohortManagerTest {
+    @Test
+    public void testAdd(){
+        CohortManager cH = new CohortManager();
+        cH.add("milk", 6);
+        Assertions.assertTrue(cH.contains("milk"));
+    }
+
+    @Test
+    public void testTotal(){
+        CohortManager cH = new CohortManager();
+        cH.add("milk", 6);
+        cH.add("water", 10);
+        cH.add("apple", 10);
+        Assertions.assertEquals(26, cH.total());
+    }
 
 }
