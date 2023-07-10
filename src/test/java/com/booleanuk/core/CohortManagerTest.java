959322db3b2a90c5fb98d99b75d8ce0f7436b320
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-10 11:54:39 +0200

    red stage of first test

--- src/test/java/com/booleanuk/core/CohortManagerTest.java
+++ src/test/java/com/booleanuk/core/CohortManagerTest.java
@@ -4,5 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CohortManagerTest {
+    @Test
+    public void whenItemIsNotInTheBasketShouldReturnTrue(){
+        Basket basket = new Basket();
+        String product = "Milk";
+        int price = 14;
 
+        boolean result = basket.add(product, price);
+
+        Assertions.assertTrue(result);
+    }
 }
