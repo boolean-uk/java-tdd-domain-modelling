85f660e4a9660a9fc7c35b025105b71365ed998a
Lev Moskaliuk <moskaluklev@gmail.com>
2023-07-10 12:09:25 +0200

    BasketTest implementation

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -1,12 +1,24 @@
 package com.booleanuk.core;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BasketTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void name() {
+
+        Basket basket = new Basket();
+        boolean check1 = basket.add("apple", 1);
+        Assertions.assertTrue(check1);
+        boolean check2 = basket.add("apple", 3);
+        Assertions.assertFalse(check2);
+
     }
 }
